56919e3a574aa57f00dfce174d7e84b119871f92
chinmaypant21 <pant.chinu.2@gmail.com>
2023-03-25 11:28:38 +0530

    Skeleton Cards fix

--- src/components/skeleton/Watch.jsx
+++ src/components/skeleton/Watch.jsx
@@ -1,12 +1,13 @@
 import WatchCard from "./WatchCard"
 import './Watch.css'
+const displayCardsLength = 10;
 
 const Watch = () => {
     return (
         <div className="watch-container">
             <div className="watch-wrapper">
             {
-                Array.from({ length: 10 }).map(() => <WatchCard />)
+                Array.from({ length: displayCardsLength }).map(() => <WatchCard />)
             }
             </div>
         </div>
